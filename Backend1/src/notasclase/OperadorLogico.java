48c7fdbfbca5640d91f78f77976fb3f4edf771c6
Bernardo A Mira García <bamira1@misena.edu.co>
2024-05-24 13:57:54 -0500

    Cambio

---
+++ Backend1/src/notasclase/OperadorLogico.java
@@ -0,0 +1,17 @@
+package notasclase;
+
+public class OperadorLogico {
+
+
+    public static void main(String[] args) {
+
+
+        String nombre;
+        String telefono;
+        String correo;
+        int clave;
+
+
+
+    }
+}
